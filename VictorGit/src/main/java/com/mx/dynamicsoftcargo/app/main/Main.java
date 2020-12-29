77974fa6073ecdd0e84de394fead4915246af8ae
VICTOR AMARO <VICTOR AMARO@LAPTOP-RMUA8114>
2020-12-29 17:30:29 -0600

    saludo a Zury

--- VictorGit/src/main/java/com/mx/dynamicsoftcargo/app/main/Main.java
+++ VictorGit/src/main/java/com/mx/dynamicsoftcargo/app/main/Main.java
@@ -22,7 +22,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Hugo");
-		System.out.println("HOLA VIC");
+		System.out.println("HOLA VIC"); 
+		System.out.println("Hola Danna");
 
 	}
 
