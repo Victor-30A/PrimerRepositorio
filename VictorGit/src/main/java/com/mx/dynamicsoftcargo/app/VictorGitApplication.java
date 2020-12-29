be477755601e09cfd09dd3c6d13d981da4c68608
VICTOR AMARO <VICTOR AMARO@LAPTOP-RMUA8114>
2020-12-29 17:11:31 -0600

    subiendo mi primer cambio

---
+++ VictorGit/src/main/java/com/mx/dynamicsoftcargo/app/VictorGitApplication.java
@@ -0,0 +1,13 @@
+package com.mx.dynamicsoftcargo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VictorGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VictorGitApplication.class, args);
+	}
+
+}
---
+++ VictorGit/src/main/java/com/mx/dynamicsoftcargo/app/main/Main.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.mx.dynamicsoftcargo.app.main;
+
+/**
+ * @author VICTOR AMARO
+ *
+ */
+public class Main {
+
+	/**
+	 * 
+	 */
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Hugo");
+
+	}
+
+}
